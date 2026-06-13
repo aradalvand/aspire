8ad77742bfb1d743de45cc69b64e9e862fe95ba6
Arad Alvand <aradalvand@gmail.com>
2026-06-14 02:08:23 +0330

    include `&directConnection=true` when `MongoDBServerResource` is part of a replica set

--- src/Aspire.Hosting.MongoDB/MongoDBBuilderExtensions.cs
+++ src/Aspire.Hosting.MongoDB/MongoDBBuilderExtensions.cs
@@ -278,6 +278,7 @@ public static class MongoDBBuilderExtensions
         ArgumentNullException.ThrowIfNull(builder);
         ArgumentException.ThrowIfNullOrEmpty(name);
 
+        builder.Resource.ReplicaSetName = name;
         return builder.WithArgs("--replSet", name);
     }
 
